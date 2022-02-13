98546cb4395524249ca8a1743b35d46a48d44236
npicciano79 <npicciano79@gmail.com>
2022-02-13 14:34:36 -0500

    javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,36 @@
-function titleImageScroll(){
-    console.log('hello');
+var slideIndex=1;
+showSlides(slideIndex);
+
+//next pervious controls
+function plusSlides(n){
+    showSlides(slideIndex+=n);
 }
+
+//thumbnail control
+function currentSlide(n){
+    showSlides(slideIndex=n);
+}
+
+function showSlides(n){
+    var i;
+    var slides=document.getElementsByClassName("carousel__slide");
+    var dots=document.getElementsByClassName("carousel__indicator");
+    if (n>slides.length){slideIndex=1}
+    if (n<1){slideIndex=slides.length}
+    for (i=0;i<slides.length;i++){
+        slides[i].style.display="none";
+    }
+    for (i=0;i<dots.length;i++){
+        dots[i].className=dots[i].className.replace(" active","");
+    }
+    slides[slideIndex-1].style.display="block";
+    dots[slideIndex-1].className+=" active";
+
+}
+
+
+
+//on left click, move sides left
+//on right click, move slides right
+//on nav indicator click, move to that slide
+
